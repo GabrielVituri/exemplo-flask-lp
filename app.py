6be65e369575468a4d0550f17fa7db64146f5dca
GabrielVituri <gabrieltozato123@hotmail.com>
2021-11-16 19:52:07 +0000

    teste

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-tarefas = [
+pizzas = [
     {"nome": "4 Queijos", "disponivel": False},
     {"nome": "Marguerita", "disponivel": False},
     {"nome": "Portuguesa", "disponivel": True},
@@ -10,7 +10,7 @@ tarefas = [
 
 @app.route('/')
 def index():
-    return render_template('index.html', lista=tarefas)
+    return render_template('index.html', lista=pizzas)
 
 @app.route('/create')
 def create():
@@ -20,14 +20,13 @@ def create():
 def save():
     nome = request.form['nome']
     disponivel = request.form['disponivel']
-
-    if "disponivel" == True: tarefa = { "nome": nome, "disponivel": True }
-    else: tarefa = { "nome": nome, "disponivel": False }
-    tarefas.append(tarefa)
+    { "nome": nome, "disponivel": True }
+    { "nome": nome, "disponivel": False }
+    pizzas.append(tarefa)
 
     return redirect('https://5000-brown-lemur-kiu2j61j.ws-us18.gitpod.io/')
 
-#jsaisjdifdsfs
+#jsaisjdifdsfsa
 
 
 
